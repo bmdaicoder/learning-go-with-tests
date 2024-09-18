ff1fa7810d6b026a766ebe02490d764368a6f5a6
bmdaicoder <bmdaicoder@gmail.com>
2024-09-18 23:11:04 +0700

    test with arrays

---
+++ arrayandslices/sum.go
@@ -0,0 +1,17 @@
+package main
+
+func Sum(arr []int) int {
+	sum := 0
+	for _, number := range arr {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
